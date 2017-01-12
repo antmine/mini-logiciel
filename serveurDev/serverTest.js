adbbe918f8e99ebdb396dddc2f112e2aba482a79
lauret_s <lauret_s@epitech.eu>
2017-01-12 20:54:43 +0100

    clear code

--- serveurDev/serverTest.js
+++ serveurDev/serverTest.js
@@ -1,40 +1,30 @@
-var express = require('express');
-var ejs = require('ejs');
 var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+
+app.set('view engine', 'ejs');
+
+app.use('/Js', express.static('../srcJs'));
+app.use('/test', express.static('../testsUnitairs'));
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+/*
 app.get('/sha256.js.mem', function(req, res){
   var str = fs.readFileSync('../srcJs/sha256.js.mem', 'utf8');
-  var ret = ejs.render(str);
-  res.send(ret);
+  res.send(str);
 });
-
+*/
 app.get('/', function(req, res){
-  var str = fs.readFileSync('./srcEJS/test.ejs', 'utf8');
-  var ret = ejs.render(str);
-  res.send(ret);
-});
-
-app.get('/test/', function(req, res){
-  var tmp = req.url.split("/");
-  tmp = tmp[tmp.length - 1].split("?");
-
-  try {
-    var str = fs.readFileSync('../testsUnitairs/'+ tmp[0], 'utf8');
-    var ret = ejs.render(str);
-  } catch (e) {
-    var str = fs.readFileSync('../testsUnitairs/unitTest.html', 'utf8');
-    var ret = ejs.render(str);
-  }
-  res.send(ret);
+  res.render('test');
 });
 
 app.get('/mini-logiciel', function(req, res){
-  var str = fs.readFileSync('./srcEJS/index.ejs', 'utf8');
-  var ret = ejs.render(str);
-  res.end(ret);
+  res.render('index');
 });
-
+/*
 
 app.get('/Js/*', function(req, res){
   var tmp = req.url.split("/");
@@ -49,5 +39,5 @@ app.get('/Js/*', function(req, res){
   res.send(ret);
 });
 
-
+*/
 app.listen(5000);
