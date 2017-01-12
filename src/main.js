9726c3071c517f0c04d0bfc7efbed029bbb05336
Aljosha Armitano <aljosha@antmine.io>
2017-01-12 18:43:13 -0500

    update some file to show what is the coding norme

---
+++ src/main.js
@@ -0,0 +1,34 @@
+/**
+ * Function main.
+ *  [EXPLAIN WHAT IS FOR]
+ */
+function main() {
+	var objData = new DataSniffer();
+    	objData.on("first", function(self) {
+       	console.log(self.info);
+    		$.post( "/data", self.info);
+  	});
+
+    	/**
+    	 *  ~Anonymous function.~
+    	 *  [EXPLAIN WHAT IS FOR].
+    	 * @param str [EXPLAIN WHAT IS IT].
+    	 */
+    	objData.on("batteryState", function(str) {
+	    	console.log(str);
+    		$.post( "/data", str);
+	});
+
+    	/**
+    	 *  ~Anonymous function.~
+    	 *  [EXPLAIN WHAT IS FOR].
+    	 * @param str [EXPLAIN WHAT IS IT].
+    	 */
+	objData.on("tabActivState", function(str) {
+		console.log(str);
+ 		$.post( "/data", str);
+  	})
+	objData.run();
+}
+
+main();
