f49926e1f8072bda6e474e6db87eae049916d0fd
Aljosha Armitano <aljosha@antmine.io>
2017-01-12 18:56:34 -0500

    fix indentation

--- src/main.js
+++ src/main.js
@@ -3,32 +3,30 @@
  *  [EXPLAIN WHAT IS FOR]
  */
 function main() {
-	var objData = new DataSniffer();
-    	objData.on("first", function(self) {
-       	console.log(self.info);
-    		$.post( "/data", self.info);
-  	});
+    var objData = new DataSniffer();
+    objData.on("first", function(self) {
+        console.log(self.info);
+        $.post("/data", self.info);
+    });
 
-    	/**
-    	 *  ~Anonymous function.~
-    	 *  [EXPLAIN WHAT IS FOR].
-    	 * @param str [EXPLAIN WHAT IS IT].
-    	 */
-    	objData.on("batteryState", function(str) {
-	    	console.log(str);
-    		$.post( "/data", str);
-	});
+		/**
+		 * 
+		 */
+    objData.on("batteryState", function(str) {
+        console.log(str);
+        $.post("/data", str);
+    });
 
-    	/**
-    	 *  ~Anonymous function.~
-    	 *  [EXPLAIN WHAT IS FOR].
-    	 * @param str [EXPLAIN WHAT IS IT].
-    	 */
-	objData.on("tabActivState", function(str) {
-		console.log(str);
- 		$.post( "/data", str);
-  	})
-	objData.run();
+    /**
+     *  ~Anonymous function.~
+     *  [EXPLAIN WHAT IS FOR].
+     * @param str [EXPLAIN WHAT IS IT].
+     */
+    objData.on("tabActivState", function(str) {
+        console.log(str);
+        $.post("/data", str);
+    })
+    objData.run();
 }
 
 main();
