3a90ce81d7c95d7838f24da75ac0af899354bb11
lauret_s <lauret_s@epitech.eu>
2017-01-16 03:39:38 +0100

    add comments in bannerHandle class

--- src/bannerHandle.js
+++ src/bannerHandle.js
@@ -1,8 +1,22 @@
+/**
+ * Class BannerHandle.
+ *  [Controle the execution scripte]
+ */
 function BannerHandle(){
+  /**
+   *  ~On function.~
+   *  [This function is used to set the callBack].
+   * @param key [key (string)].
+   * @param callB [callBack].
+   */
   this.on = function(key, callB) {
     this.callBack[key] = callB;
   }
 
+  /**
+   *  ~On function.~
+   *  [This function is used to display the banner and link the callBack with the button event].
+   */
   this.display = function(){
     var bannerCode = '<div id="antmine_content">' + this.htmlBanner + this.cssBanner + '</div>';
     console.log(bannerCode);
@@ -23,4 +37,6 @@ function BannerHandle(){
   this.callBack = {};
   this.callBack["execScript"] = function(){};
   this.callBack["stopScript"] = function(){};
+
+  return this;
 }
