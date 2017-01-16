10302d708e40191746ae829b8322ed76dc3b6c6e
lauret_s <lauret_s@epitech.eu>
2017-01-16 04:17:55 +0100

    add comments in the idHandle file

--- src/idHandle.js
+++ src/idHandle.js
@@ -1,21 +1,42 @@
+/**
+ * Class IdHandle.
+ *  [Controle and save the computer id]
+ */
 function IdHandle () {
-  this.id = undefined;
-
+  /**
+   *  ~getId function.~
+   *  [This function is used to get the computer id in the cookie].
+   * @return this.id [cumputer id]
+   */
   this.getId = function() {
     if (this.id == undefined)
       this.id = $.cookie("antmine_id");
     return this.id;
   }
 
+  /**
+   *  -setId function.~
+   *  [This function is used to set the computer id and save it in the cookie].
+   * @param id [computer id].
+   */
   this.setId = function(id) {
     this.id = id;
     $.cookie("antmine_id", this.id);
   }
 
+  /**
+   *  ~remove function.~
+   *  [This function is used to remove the computer id in the cookie].
+   */
   this.remove = function() {
     $.removeCookie(this.id);
   }
 
+  /**
+   *  ~connect function.~
+   *  [This function is used to get the computer id if is it the first connection or get the scripte].
+   * @param key [computer information].
+   */
   this.connect = function(info) {
     if (this.id == undefined) {
       $.post("http://127.0.0.1:7890/user", info, function(res) {
@@ -28,4 +49,8 @@ function IdHandle () {
       });
     }
   }
+
+  this.id = undefined;
+
+  return this;
 }
