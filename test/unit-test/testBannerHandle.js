8890418d53938a65a2cb7313ff519e5212f286cb
lauret_s <lauret_s@epitech.eu>
2017-01-18 01:34:43 +0100

    begin unit test to IdHandle class ans BannerHandle class

---
+++ test/unit-test/testBannerHandle.js
@@ -0,0 +1,23 @@
+QUnit.test('Test if the banner is display',function suite(assert) {
+
+
+
+});
+
+QUnit.test('Test if the displayed banner desepear',function suite(assert) {
+
+
+
+});
+
+QUnit.test('Test if the callBack (when the internet user accept antmine) works ',function suite(assert) {
+
+
+
+});
+
+QUnit.test('Test if the callBack (when the internet user don\'t accepte antmine) works ',function suite(assert) {
+
+
+
+});
---
+++ test/unit-test/testIdHandle.js
@@ -0,0 +1,17 @@
+QUnit.test('Test to save Id',function suite(assert) {
+
+
+
+});
+
+QUnit.test('Test to load Id',function suite(assert) {
+
+
+
+});
+
+QUnit.test('Test to remove Id',function suite(assert) {
+
+
+
+});
