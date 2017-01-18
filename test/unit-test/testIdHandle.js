403e065b9b1b96476963b08562582d948d432520
lauret_s <lauret_s@epitech.eu>
2017-01-18 01:53:03 +0100

    test IdHandle class

--- test/unit-test/testIdHandle.js
+++ test/unit-test/testIdHandle.js
@@ -1,17 +1,79 @@
 QUnit.test('Test to save Id',function suite(assert) {
+  $.removeCookie("antmine_id");
+  var objIdHandle = new IdHandle();
 
+  objIdHandle.setId("test");
 
-
+  var strTest = $.cookie("antmine_id");
+  assert.equal(strTest, "test", "");
 });
 
 QUnit.test('Test to load Id',function suite(assert) {
+  $.removeCookie("antmine_id");
+  $.cookie("antmine_id", "test")
+  var objIdHandle = new IdHandle();
 
+  var strTest = objIdHandle.getId();
 
+  assert.equal(strTest, "test", "");
+  assert.equal(objIdHandle.id, "test", "");
+});
 
+QUnit.test('Test to load Id bis',function suite(assert) {
+  $.removeCookie("antmine_id");
+  var objIdHandle = new IdHandle();
+
+  var strTest = objIdHandle.getId();
+
+  assert.equal(strTest, undefined, "");
+  assert.equal(objIdHandle.id, undefined, "");
 });
 
 QUnit.test('Test to remove Id',function suite(assert) {
+  $.removeCookie("antmine_id");
+  $.cookie("antmine_id", "test")
+  var objIdHandle = new IdHandle();
 
+  var strTest = objIdHandle.getId();
 
+  assert.equal(objIdHandle.id, "test", "");
+
+  objIdHandle.remove();
+
+  var strTest = $.cookie("antmine_id");
+  assert.equal(strTest, undefined, "");
+  assert.equal(objIdHandle.id, undefined, "");
+});
+
+QUnit.test('Test all',function suite(assert) {
+  var objIdHandle = new IdHandle();
+  var strTest = "";
+
+  objIdHandle.remove();
+
+  strTest = $.cookie("antmine_id");
+  assert.equal(strTest, undefined, "");
+  assert.equal(objIdHandle.id, undefined, "");
+
+  strTest = objIdHandle.getId();
+
+  assert.equal(strTest, undefined, "");
+  assert.equal(objIdHandle.id, undefined, "");
+
+  objIdHandle.setId("test");
+
+  strTest = $.cookie("antmine_id");
+  assert.equal(strTest, "test", "");
+
+  strTest = objIdHandle.getId();
+
+  assert.equal(strTest, "test", "");
+  assert.equal(objIdHandle.id, "test", "");
+
+  objIdHandle.remove();
+
+  strTest = $.cookie("antmine_id");
+  assert.equal(strTest, undefined, "");
+  assert.equal(objIdHandle.id, undefined, "");
 
 });
