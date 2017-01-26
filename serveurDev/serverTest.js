8af42dc81db2bab9784bd2b881b602bb4f967a38
lauret_s <lauret_s@epitech.eu>
2017-01-26 22:31:33 +0100

    change git module by npm module (jquery.cookie)

--- serveurDev/serverTest.js
+++ serveurDev/serverTest.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 
 app.use('/Js', express.static('../src'));
+app.use('/Js', express.static('../src/node_modules'));
 app.use('/unitTest', express.static('../test/unit-test'));
 
 app.get('/', function(req, res){
